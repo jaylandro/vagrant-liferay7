415e365523a1c8afc81e90358cbfc0fc46a3503c
james landro <jaylandro@gmail.com>
2014-11-04 16:55:57 -0600

    Configuration update

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -12,7 +12,7 @@ class mysql {
     require => Package["mysql-server"]
   }
 
-  $password = "password"
+  $password = "eivnc7UCo2"
 
   exec {"mysql-passwd":
     unless => "mysqladmin -uroot -p${password} status",
@@ -23,7 +23,7 @@ class mysql {
 
   $l_db = "lportal"
   $l_user = "liferay"
-  $l_pass = "password"
+  $l_pass = "kdnv7FKc3"
 
   exec {"create-liferay-db":
     unless => "mysql -u${l_user} -p${l_pass} ${l_db}",
